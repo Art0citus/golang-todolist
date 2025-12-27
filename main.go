4abb730c357b1c1491c8f558c9b729b09d09ba93
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2025-12-27 16:10:46 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 )
 
 func main() {
-	// saare CLI arguments ek slice me milenge
+	
 	args := os.Args
 
-	// slice ko print kar ke dekh lo
 	fmt.Println(args)
 
-	// Example access
+	
 	if len(args) > 1 {
 		fmt.Println("First argument:", args[1])
 	}
